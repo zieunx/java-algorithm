1848ed12fee326c0f489f9d1e305527a2e77fa44
zieunx <jelee@12cm.co.kr>
2020-04-06 23:15:04 +0900

    bj n10810 success

---
+++ src/beakjoon/n10818.java
@@ -0,0 +1,26 @@
+package beakjoon;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class n10818 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = sc.nextInt();
+        int min = max;
+
+        for (int i=1 ; i<n ; i++) {
+            int num = sc.nextInt();
+            if (max < num) {
+                max = num;
+            } else
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        System.out.println(min +  " " + max);
+    }
+}
