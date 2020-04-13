283515f1f31b01a1daa19d6eb779da324de87358
zieunx <wldms_1217@naver.com>
2020-04-13 15:39:38 +0900

    n1712 success

---
+++ src/beakjoon/n1712.java
@@ -0,0 +1,19 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class n1712 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if (C <= B) {
+            System.out.println(-1);
+        } else {
+            System.out.println((int) Math.floor(((double) A / (double) (C-B))+1));
+        }
+    }
+}
