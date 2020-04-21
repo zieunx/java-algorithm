c6ecfb553d883e9859b1d7f9b3ad9d324938c8e4
zieunx <wldms_1217@naver.com>
2020-04-21 17:48:09 +0900

    n10828 클래스 빼봤는데 메모리, 시간 차이 없음ㅠ

--- src/beakjoon/n10828.java
+++ src/beakjoon/n10828.java
@@ -2,12 +2,16 @@ package beakjoon;
 
 import java.util.Scanner;
 public class n10828 {
+    static int[] array;
+    static int top;
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        Stack stack = new Stack(N); // 스택 초기화
+        array = new int[N]; // 스택 초기화
+        top = -1;
         sc.nextLine();
 
         String line;
@@ -15,41 +19,31 @@ public class n10828 {
             line = sc.nextLine();
             switch (line.substring(0,3)) {
                 case "pus":
-                    stack.push(Integer.parseInt(line.split(" ")[1]));
+                    push(Integer.parseInt(line.split(" ")[1]));
                     break;
                 case "pop":
-                    System.out.println(stack.pop());
+                    System.out.println(pop());
                     break;
                 case "siz":
-                    System.out.println(stack.size());
+                    System.out.println(size());
                     break;
                 case "emp":
-                    System.out.println(stack.empty());
+                    System.out.println(empty());
                     break;
                 case "top":
-                    System.out.println(stack.top());
+                    System.out.println(top());
                     break;
             }
             N--;
         }
     }
-}
 
-class Stack {
-    int[] array;
-    int top;
-
-    public Stack(int n) {
-        this.array = new int[n];
-        top = -1;
-    }
-
-    void push(int x) {
+    public static void push(int x) {
         top++;
         array[top] = x;
     }
 
-    int pop() {
+    public static int pop() {
         if (top < 0) {
             return -1;
         } else {
@@ -58,21 +52,21 @@ class Stack {
         }
     }
 
-    int size() {
+    public static int size() {
         return top+1;
     }
 
-    int empty() {
+    public static int empty() {
         if (top < 0) {
             return 1;
         }
         return 0;
     }
 
-    int top() {
+    public static int top() {
         if (top < 0) {
             return -1;
         }
-        return this.array[top];
+        return array[top];
     }
 }
