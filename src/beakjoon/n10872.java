6c065bcadab587bf98e6be6be4518bc1033ac2ad
zieunx <wldms_1217@naver.com>
2020-05-30 23:31:50 +0900

    10872 success

--- src/beakjoon/n10872.java
+++ src/beakjoon/n10872.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class n10872 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-
-        for (int i=2; i<= N; i++) {
-
-        }
+        
+        System.out.println(getResult(sc.nextInt()));
         sc.close();
+    }
+    static long getResult (long n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * getResult(n-1);
     }
 }
