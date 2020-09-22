0484f180ca915611fd63d1ab5082a2830969cea0
jelee <jelee@12cm.co.kr>
2020-09-22 18:18:24 +0900

    modify files

--- src/beakjoon/n1874.java
+++ src/beakjoon/n1874.java
@@ -5,24 +5,25 @@ import java.util.Stack;
 
 public class n1874 {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        Stack<Integer> stack = new Stack<>();
-        int N = 0;
-        N = Integer.parseInt(reader.readLine());
-
-        int[] arr = new int[N];
-
-        int i=1;
-
-        while(true) {
-            stack.push(i);
-            if (i);
-        }
-
-        reader.close();
-        writer.flush();
-        writer.close();
+        // TODO
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        Stack<Integer> stack = new Stack<>();
+//        int N = 0;
+//        N = Integer.parseInt(reader.readLine());
+//
+//        int[] arr = new int[N];
+//
+//        int i=1;
+//
+//        while(true) {
+//            stack.push(i);
+//            if (i);
+//        }
+//
+//        reader.close();
+//        writer.flush();
+//        writer.close();
     }
 }
