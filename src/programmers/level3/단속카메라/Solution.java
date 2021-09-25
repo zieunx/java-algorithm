b314d13da7dde329f770b29f93c054017006f78e
zieunx <wldms_1217@naver.com>
2021-09-25 23:51:35 +0900

    fix(프로그래머스/level3/단속카메라): 코드 reset

--- src/programmers/level3/단속카메라/Solution.java
+++ src/programmers/level3/단속카메라/Solution.java
@@ -14,32 +14,7 @@ public class Solution {
 
     public static int solution(int[][] routes) {
         int answer = 0;
-        List<Integer> spots = new ArrayList<>();
-        Map<Integer, List<Integer>> countAndCarIndexMap = new HashMap<>();
-
-        // 구간별 마주치는 차량 정리
-        for (int i = 0; i < routes.length; i++) {
-            for (int j = 0; j < 2; j++) {
-                spots.add(routes[i][j]);
-            }
-        }
-        spots = spots.stream().sorted().collect(Collectors.toList());
-
-        for (Integer spot : spots) {
-            countAndCarIndexMap.put();
-            spot
-        }
-
-        spots
-
-
-        // 구간별 마주치는 차량 정리
-        // 차량수 큰순 정렬
-        // 큰수부터...
-
-        while(!isFindAllCar) {
-
-        }
+        // reset
 
         return answer;
     }
