2d77aee80767f6f94e4f3c01c629d89b89527376
zieunx <wldms_1217@naver.com>
2022-04-15 01:30:42 +0900

    fix(프로그래머스/level2/메뉴리뉴얼): 시간초과 대응. ing

--- src/main/java/com/study/javaalgorithm/programmers/level2/메뉴리뉴얼/Solution.java
+++ src/main/java/com/study/javaalgorithm/programmers/level2/메뉴리뉴얼/Solution.java
@@ -8,25 +8,30 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Solution {
-	public List<String> solution(String[] orders, int[] course) {
-		List<String> newMenuList = new ArrayList<>();
+	List<String> newMenuList = new ArrayList<>();
 
-		Map<String, Integer> ranking = new HashMap<>();
-		Set<String> menusByOrder = new HashSet<>();
+	Map<String, Integer> ranking = new HashMap<>();
+	Set<String> menusByOrder = new HashSet<>();
+	int max = 0;
+
+	public List<String> solution(String[] orders, int[] course) {
+		// List<String> newMenuList = new ArrayList<>();
+		//
+		// Map<String, Integer> ranking = new HashMap<>();
+		// Set<String> menusByOrder = new HashSet<>();
+		//
 		for (int size : course) {
-			int max = 0;
+			max = 0;
 			for (String order : orders) {
 				if (order.length() < size) {
 					continue;
 				}
-				// System.out.println("==== {" + order + " / size = " + size + "} ====");
 				menusByOrder.clear();
-				for (int i = 0; i < order.length(); i++) {
-					combineMenu(menusByOrder, order, i, size, "");
-				}
+
+				combineMenu(menusByOrder, order, 0, size, "");
+
 				for (String menu : menusByOrder) {
 					// System.out.println("menu ! " + menu);
 					int count = ranking.containsKey(menu) ? ranking.get(menu) + 1 : 1;
@@ -35,16 +40,13 @@ public class Solution {
 					max = Math.max(count, max);
 				}
 			}
-			// ranking.keySet()
-			// 	.forEach(key -> System.out.println("key: " + key + " / count: " + ranking.get(key)));
 
-			final int finalMax = max;
-
-			if (finalMax != 1) {
-				newMenuList.addAll(ranking.keySet()
-					.stream()
-					.filter(key -> finalMax == ranking.get(key))
-					.collect(Collectors.toList()));
+			if (max != 1) {
+				for (String newMenu : ranking.keySet()) {
+					if (max == ranking.get(newMenu)) {
+						newMenuList.add(newMenu);
+					}
+				}
 			}
 
 			ranking.clear();
@@ -54,22 +56,19 @@ public class Solution {
 		return newMenuList;
 	}
 
-	public void combineMenu(Set<String> distinctMenu, String order, int index, int setMenuSize, String menu) {
+	public void combineMenu(Set<String> menusByOrder, String order, int index, int setMenuSize, String menu) {
 		if (setMenuSize == menu.length()) {
 			char[] chars = menu.toCharArray();
 			Arrays.sort(chars);
-			distinctMenu.add(String.join("", String.join(",", new String(chars))));
-			return;
-		}
-		if (index == order.length() - 1) {
+			menusByOrder.add(new String(chars));
 			return;
 		}
 		for (int i = index; i < order.length(); i++) {
 			if (menu.contains(String.valueOf(order.charAt(i)))) {
 				continue;
 			}
-			// System.out.println("menu: " + String.join(",", menu) + " / i = " + i);
-			combineMenu(distinctMenu, order, index, setMenuSize, menu + order.charAt(i));
+			// System.out.println("menu: " + menu + " / i = " + i);
+			combineMenu(menusByOrder, order, index, setMenuSize, menu + order.charAt(i));
 		}
 	}
 }
